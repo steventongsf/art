7cf7bc9289675257c21a27aaf62a131ba5d25afb
Steven Tong <steventongsf@gmail.com>
2025-01-24 23:44:12 -0800

    refactored gallery component

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import Home from "./pages/Home"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Link as RouterLink } from "react-router-dom";
 import PageHeader from "./components/PageHeader"
+import json from "./data/fans-book1.json"
+import PhotoGallery from './pages/PhotoGallery'
 
 function App() {
   return (
@@ -17,7 +19,11 @@ function App() {
           <PageHeader />  
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/fans-book1" element={<FansBook1 />} />
+            <Route path="/fans-book1" 
+              element={<PhotoGallery 
+              title="Chinese Fans Book 1" 
+              description="Collection of 42 chinese fan paintings from the early 19th to early 20th centuries." 
+              json={json} />} />
             <Route path="/fans-book2" element={<FansBook2 />} />
           </Routes>
         </BrowserRouter>
