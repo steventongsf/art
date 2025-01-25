04c1e5d80fac8f421338e9b8cc6b3c8605c1ffdd
Steven Tong <steventongsf@gmail.com>
2025-01-24 23:28:38 -0800

    refactor modal dialog

--- src/components/MyModal.jsx
+++ src/components/MyModal.jsx
@@ -1,4 +1,22 @@
+import { useEffect } from "react";
+
 function MyModal() {
+    useEffect(() => {
+        const closeButton = document.querySelector(".close");
+        closeButton.onclick = function () {
+        console.log("myModal close clicked")
+        const modal = document.getElementById("myModal");
+        modal.style.display = "none";
+        };
+        document.addEventListener("keydown", function (event) {
+            if (event.key === "Escape") {
+              const modal = document.getElementById("myModal");
+              if (modal.style.display === "block") {
+                modal.style.display = "none";
+              }
+            }
+          });
+    });
     return (    
     <div id="myModal" className="modal">
         <span className="close">&times;</span>
--- src/pages/FansBook1.jsx
+++ src/pages/FansBook1.jsx
@@ -27,21 +27,7 @@ function FansBook1() {
         });
       });
       
-      const closeButton = document.querySelector(".close");
-      closeButton.onclick = function () {
-        console.log("myModal close clicked")
-        const modal = document.getElementById("myModal");
-        modal.style.display = "none";
-      };
 
-      document.addEventListener("keydown", function (event) {
-        if (event.key === "Escape") {
-          const modal = document.getElementById("myModal");
-          if (modal.style.display === "block") {
-            modal.style.display = "none";
-          }
-        }
-      });
     
     });
   });
